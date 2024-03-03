e8af02fae74abdc6999ded6ca0cb42fe62f2633f
Manu Castrillon <10585946+ManuCastrillonM@users.noreply.github.com>
2024-03-03 16:48:19 +0100

    Update hero copy & styles (#16)
    update hero copy & styles

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,15 +1,23 @@
-import hero from "../../assets/hero.jpg";
+import heroBackground from "../../assets/hero.jpg";
 import "./hero.scss";
 
 export const Hero = (): JSX.Element => {
+  const content = {
+    title: "Guardianes Ancestrales",
+    subtitle:
+      "Fortaleciendo el futuro de la comunidad indigena Wayuu a través de la educación y la tradición.",
+  };
+
   return (
-    <section className="py-16 hero flex items-center">
-      <img className="hero__background" src={hero} alt="" aria-hidden="true" />
-      <div className="container">
-        <h1 className="hero__title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc
-          magna, suscipit ut tellus ut, pulvinar vestibulum justo.
-        </h1>
+    <section className="hero">
+      <img
+        className="hero__background"
+        src={heroBackground}
+        aria-hidden="true"
+      />
+      <div className="hero__text-container">
+        <h1 className="hero__title">{content.title}</h1>
+        <h2 className="hero__subtitle">{content.subtitle}</h2>
       </div>
     </section>
   );
