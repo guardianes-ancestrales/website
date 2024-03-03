f971f8501f139a29b58da045ef7c27e46a42a945
manuCastrillonM <10585946+ManuCastrillonM@users.noreply.github.com>
2024-03-03 16:28:59 +0100

    refactor styles

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -7,15 +7,16 @@ export const Hero = (): JSX.Element => {
     subtitle:
       "Fortaleciendo el futuro de la comunidad indigena Wayuu a través de la educación y la tradición.",
   };
+
   return (
-    <section className="py-16 hero flex items-center">
+    <section className="hero">
       <img
         className="hero__background"
         src={heroBackground}
         aria-hidden="true"
       />
-      <div className="mr-6 ml-6 md:mr-8 md:ml-8 hero__text-container">
-        <h1 className="mb-4 drop-shadow-md hero__title">{content.title}</h1>
+      <div className="hero__text-container">
+        <h1 className="hero__title">{content.title}</h1>
         <h2 className="hero__subtitle">{content.subtitle}</h2>
       </div>
     </section>
